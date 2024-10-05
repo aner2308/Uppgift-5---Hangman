4a51166f8582ed17af4057c4616371c168cad139
Anton <erikssonanton94@gmail.com>
2024-10-05 04:34:05 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hangman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Välkommen till Hangman!");
+        }
+    }
+}
